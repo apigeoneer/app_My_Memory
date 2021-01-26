e91b8369b8cf0b70d38600589228569d9a76419b
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-01-26 18:55:27 +0530

    removed hard-coded width & height  of the grid items. made them occupy the whole width & height of the parent.

--- app/src/main/java/com/gmail/apigeoneer/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/gmail/apigeoneer/mymemory/MemoryBoardAdapter.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(
         private val context: Context,
@@ -12,7 +14,15 @@ class MemoryBoardAdapter(
         RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width / 2
+        val cardHeight = parent.height / 4
+        val cardSideLength = min(cardWidth, cardHeight)
+
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.card_view).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+
         return ViewHolder(view)
     }
 
