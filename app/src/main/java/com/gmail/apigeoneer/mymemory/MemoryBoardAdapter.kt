b05ed08028c12fc4725c5f1a1c7d0f76a890f3a2
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-01-26 19:41:37 +0530

    added a click listener to grid items, logging grid item position.

--- app/src/main/java/com/gmail/apigeoneer/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/gmail/apigeoneer/mymemory/MemoryBoardAdapter.kt
@@ -1,9 +1,11 @@
 package com.gmail.apigeoneer.mymemory
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.min
@@ -13,15 +15,21 @@ class MemoryBoardAdapter(
         private val numPieces: Int) :
         RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val cardWidth = parent.width / 2
-        val cardHeight = parent.height / 4
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
         val cardSideLength = min(cardWidth, cardHeight)
 
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
-        val layoutParams = view.findViewById<CardView>(R.id.card_view).layoutParams
+        val layoutParams = view.findViewById<CardView>(R.id.card_view).layoutParams as ViewGroup.MarginLayoutParams
         layoutParams.width = cardSideLength
         layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
 
         return ViewHolder(view)
     }
@@ -33,8 +41,12 @@ class MemoryBoardAdapter(
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.image_button)
+
         fun bind(position: Int) {
-            // No-op
+            imageButton.setOnClickListener {
+                Log.i(TAG, "Clicked on position $position")
+            }
         }
 
     }
