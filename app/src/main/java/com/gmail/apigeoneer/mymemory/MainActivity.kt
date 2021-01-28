8bdfd88a22f1b70af31b7880d8cb63f54fe031b9
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-01-28 14:40:37 +0530

    Changed the numMoves & numPairs text to be specific to the chosen board size.

--- app/src/main/java/com/gmail/apigeoneer/mymemory/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/mymemory/MainActivity.kt
@@ -86,6 +86,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupBoard() {
+        when (boardSize) {
+            BoardSize.EASY -> {
+                tvNumMoves.text = "Easy: 4 x 2"
+                tvNumPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = "Medium: 6 x 3"
+                tvNumPairs.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "Hard: 6 x 4"
+                tvNumPairs.text = "Pairs: 0 / 12"
+            }
+        }
+
         tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
         memoryGame = MemoryGame(boardSize)
         adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
