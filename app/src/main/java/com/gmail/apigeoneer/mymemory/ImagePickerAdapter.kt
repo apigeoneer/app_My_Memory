27f47bbb884f95909a5347998ff89a10f65e60fb
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-01-28 19:54:31 +0530

    Created the image grid to select images from the gallery.

--- app/src/main/java/com/gmail/apigeoneer/mymemory/ImagePickerAdapter.kt
+++ app/src/main/java/com/gmail/apigeoneer/mymemory/ImagePickerAdapter.kt
@@ -5,28 +5,57 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.gmail.apigeoneer.mymemory.models.BoardSize
+import kotlin.math.min
 
 class ImagePickerAdapter(
     private val context: Context,
-    private val chosenImageUris: List<Uri>,
+    private val imageUris: List<Uri>,
     private val boardSize: BoardSize
 ) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
+
+        val cardWidth = parent.width / boardSize.getWidth() - (4 * MARGIN_SIZE)
+        val cardHeight = parent.height / boardSize.getHeight() - (4 * MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+        val layoutParams = view.findViewById<ImageView>(R.id.custom_image_view).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int = boardSize.getNumPairs()
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if (position < imageUris.size) {
+            holder.bind(imageUris[position])
+        } else {
+            holder.bind()
+        }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // No-op
+        private val ivCustomImage = itemView.findViewById<ImageView>(R.id.custom_image_view)
+
+        fun bind(uri: Uri) {
+            ivCustomImage.setImageURI(uri)
+            ivCustomImage.setOnClickListener(null)
+        }
+
+        fun bind() {
+            ivCustomImage.setOnClickListener {
+                // Launch intent for user to select photos
+            }
+        }
     }
 
 }
